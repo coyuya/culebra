30feee3f4c48becaf97411d08141b1945a3216b8
coyuya <54635711+coyuya@users.noreply.github.com>
2021-03-15 13:29:27 +0100

    Add input

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use piston_window::*;
 
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
 struct Game {
     culebra: Culebra,
 }
 
 struct Culebra {
+    direction: Direction,
     pos_x: f64,
     pos_y: f64,
 }
@@ -18,12 +26,22 @@ impl Culebra {
             gl,
         );
     }
+
+    fn update(&mut self) {
+        match self.direction {
+            Direction::Up => self.pos_y -= 1.0,
+            Direction::Down => self.pos_y += 1.0,
+            Direction::Left => self.pos_x -= 1.0,
+            Direction::Right => self.pos_x += 1.0,
+        }
+    }
 }
 
 impl Game {
     fn new() -> Self {
         Game {
             culebra: Culebra {
+                direction: Direction::Right,
                 pos_x: 0.0,
                 pos_y: 0.0,
             },
@@ -31,12 +49,22 @@ impl Game {
     }
 
     fn update(&mut self) {
-        self.culebra.pos_x += 1.0;
+        self.culebra.update();
     }
 
     fn draw(&self, gl: &mut G2d, ctx: &Context) {
         clear([1.0; 4], gl);
         self.culebra.draw(gl, ctx);
+    }
+
+    fn input(&mut self, key: &Key) {
+        match key {
+            Key::W => self.culebra.direction = Direction::Up,
+            Key::S => self.culebra.direction = Direction::Down,
+            Key::A => self.culebra.direction = Direction::Left,
+            Key::D => self.culebra.direction = Direction::Right,
+            _ => {}
+        }
     }
 }
 
@@ -49,7 +77,7 @@ fn main() {
 
     let mut game = Game::new();
 
-    window.set_ups(10);
+    window.set_ups(60);
 
     while let Some(event) = window.next() {
         // render
@@ -59,6 +87,12 @@ fn main() {
             });
         }
 
+        // input
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.input(&key);
+        }
+
+        // update
         if let Some(_args) = event.update_args() {
             game.update();
         }
